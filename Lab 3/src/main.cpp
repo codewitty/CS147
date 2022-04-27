3c55ffc2040fc7f6ce9f7f72a8537e1b1838ff08
Stephanie Marie Estanislao <57146858+stephanieestanislao@users.noreply.github.com>
2022-04-27 13:58:05 -0700

    Added sensor code

--- Lab 3/src/main.cpp
+++ Lab 3/src/main.cpp
@@ -1,23 +1,41 @@
 #include <Arduino.h>
 #include <WiFi.h>
 #include <HttpClient.h>
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+
+#define DHT_PIN 2 // digital pin connected to the DHT sensor
+
+// Uncomment whatever type you're using!
+//#define DHTTYPE DHT11   // DHT 11
+#define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
+//#define DHTTYPE DHT21   // DHT 21 (AM2301)
+
+// Connect pin 1 (on the left) of the sensor to +5V
+// NOTE: If using a board with 3.3V logic like an Arduino Due connect pin 1
+// to 3.3V instead of 5V!
+// Connect pin 2 of the sensor to whatever your DHTPIN is
+// Connect pin 3 (on the right) of the sensor to GROUND (if your sensor has 3 pins)
+// Connect pin 4 (on the right) of the sensor to GROUND and leave the pin 3 EMPTY (if your sensor has 4 pins)
+// Connect a 10K resistor from pin 2 (data) to pin 1 (power) of the sensor
 
 
-// This example downloads the URL "http://arduino.cc/"
-
-char ssid[] = "";    // your network SSID (name) 
-char pass[] = ""; // your network password (use for WPA, or use as key for WEP)
+char ssid[] = "[YOUR_SSID]";    // your network SSID (name) FIXME
+char pass[] = "[YOUR_PASSWORD]"; // your network password (use for WPA, or use as key for WEP) FIXME
 
 // Name of the server we want to connect to
-const char kHostname[] = "worldtimeapi.org";
+const char kHostname[] = "worldtimeapi.org";  // FIXME
 // Path to download (this is the bit after the hostname in the URL
 // that you want to download
-const char kPath[] = "/api/timezone/Europe/London.txt";
+const char kPath[] = "/api/timezone/Europe/London.txt"; // FIXME
 
 // Number of milliseconds to wait without receiving any data before we give up
 const int kNetworkTimeout = 30*1000;
 // Number of milliseconds to wait if no data is available before trying again
 const int kNetworkDelay = 1000;
+
+// Initialize DHT sensor.
+cDHT dht(DHTPIN, DHTTYPE);
 
 void setup() {
   Serial.begin(9600);
@@ -42,9 +60,44 @@ void setup() {
   Serial.println(WiFi.localIP());
   Serial.println("MAC address: ");
   Serial.println(WiFi.macAddress());
+
+  dht.begin();
 }
 
 void loop() {
+  // Wait a few seconds between measurements.
+  delay(2000);
+
+  // Reading temperature or humidity takes about 250 milliseconds!
+  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+  float h = dht.readHumidity();
+  // Read temperature as Celsius (the default)
+  float t = dht.readTemperature();
+  // Read temperature as Fahrenheit (isFahrenheit = true)
+  float f = dht.readTemperature(true);
+
+  // Check if any reads failed and exit early (to try again).
+  if (isnan(h) || isnan(t) || isnan(f)) {
+    Serial.println(F("Failed to read from DHT sensor!"));
+    return;
+  }
+
+  // Compute heat index in Fahrenheit (the default)
+  float hif = dht.computeHeatIndex(f, h);
+  // Compute heat index in Celsius (isFahreheit = false)
+  float hic = dht.computeHeatIndex(t, h, false);
+
+  Serial.print(F("Humidity: "));
+  Serial.print(h);
+  Serial.print(F("%  Temperature: "));
+  Serial.print(t);
+  Serial.print(F("°C "));
+  Serial.print(f);
+  Serial.print(F("°F  Heat index: "));
+  Serial.print(hic);
+  Serial.print(F("°C "));
+  Serial.print(hif);
+  Serial.println(F("°F"));
 
   int err =0;
   
@@ -121,4 +174,5 @@ void loop() {
 
   // And just stop, now that we've tried a download
   while(1);
+}
 }
