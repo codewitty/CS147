ca3f8e3a27c0a4dee9a7d9e7882cba4d2fb3607f
Joshua G <gomesjoshua@gmail.com>
2022-05-24 19:55:08 -0700

    Update main.cpp

--- Plant_Parent/src/main.cpp
+++ Plant_Parent/src/main.cpp
@@ -2,10 +2,14 @@
 #include <TFT_eSPI.h>
 #include <Wire.h>
 
+void soilMoistureSensor();
+void uvSensor();
+int avgAnalogRead(int);
+
 // FIXME:
-#define MoistureSensor 0 // FIXME: replace with sensor pin
-#define UVOUT 1 //FIXME
-#define REF_3v3 2 //FIXME
+#define MoistureSensor  39
+#define UVIN 37 
+#define REF_3v3 38
 
 // never comment, needed for everything...
 TFT_eSPI tft = TFT_eSPI();  // Invoke library, pins defined in User_Setup.h
@@ -17,7 +21,7 @@ int refValue = 0;
 int uvValue = 0;
 
 float outputVoltage = 0;
-float uvIntesity = 0;
+float uvIntensity = 0;
 
 
 void setup() { 
@@ -27,7 +31,7 @@ void setup() {
   tft.fillScreen(TFT_BLACK);
   tft.setTextColor(TFT_GREEN, TFT_BLACK);  // Adding a black background colour erases previous text automatically
 
-  pinMode(UVOUT, INPUT);
+  pinMode(UVIN, INPUT);
 } 
 
 
@@ -36,6 +40,7 @@ void loop() {
   uvSensor();
   delay(30); 
 } 
+
 
 void soilMoistureSensor(){
   // soil moisture
@@ -50,28 +55,18 @@ void soilMoistureSensor(){
 
 }
 
+
 void uvSensor(){
-  // uv sensor
-  uvValue = avgAnalogRead(UVOUT);
-  refValue = avgAnalogRead(REF_3v3);
+
+  uvValue = analogRead(UVIN);
+  refValue = analogRead(REF_3v3);
 
   outputVoltage = 3.3 / refValue * uvValue;
-  uvIntesity = (outputVoltage - 0.99) * (15) / (2.8 - 0.99);
+  uvIntensity = (outputVoltage - 0.99) * (15) / (2.8 - 0.99);
 
   Serial.print("UV Sensor: ");
   Serial.println(uvValue);
 
   Serial.print("UV Intensity: ");
-  Serial.println(uvIntesity);  
-}
-
-int avgAnalogRead(int pin){
-  byte numReadings = 0;
-  unsigned int runningValue = 0;
-
-  for (int x=0; x<numReadings; x++)
-    runningValue += analogRead(pin);
-  runningValue /= numReadings;
-
-  return (runningValue);
+  Serial.println(uvIntensity);  
 }
