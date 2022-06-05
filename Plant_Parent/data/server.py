c1e73eebfd64769bb8d2b97dae903830da9bc819
Ubuntu <ubuntu@ip-172-31-3-199.ec2.internal>
2022-06-05 21:00:21 +0000

    Final iteration

--- Plant_Parent/data/server.py
+++ Plant_Parent/data/server.py
@@ -10,7 +10,6 @@ app = Flask(__name__)
 def result():
    data = []
    moisture = str(request.values.get("Soil_Moisture"))
-   print(f'Moisture: {moisture}')
    data.append(moisture)
    UV = str(request.values.get("UV_Sensor"))
    data.append(UV)
@@ -28,40 +27,47 @@ def result():
    response2 = requests.get(string, headers=headers)
    rain = []
    uv = []
+   data2 = [[0,0]]
+   data3 = []
    if response.status_code == 200:
        pred = response.json()
        weather = pred['weather']
-       #rain.append(weather[0]['main'])
-       rain.append("Clear")
-       print(f'Rain: {rain[0]}')
+       rain.append(weather[0]['main'])
+       #rain.append("Clear")
+       #print(f'Rain: {rain[0]}')
    if response2.status_code == 200:
        pred = response2.json()
        print(pred)
        #uv.append(pred['result']['uv_max'])
-       uv.append(8.0)
-       print(uv)
-   if request.method == 'GET':
-        data = [[0,0]]
-        with open('data.txt') as f:
-            for i in f:
-                if i == "\n":
-                    continue
-                i_strip = i.rstrip("\n")
-                d = list(i_strip.split(','))
-                if d[1] == "" or d[1] == 'None' or d[0] == "" or d[0] == "None":
-                    continue
-                d[0] = float(d[0])
-                d[1] = float(d[1])
-                data.append(d)
-        data2 = []
-        data2.append(data[-1])
-        return render_template("results.html",data = data2, rain = rain, uv = uv)
+       uv.append(4.0)
+       #print(uv)
+   #if request.method == 'GET':
+   data = [[0,0]]
+   with open('data.txt') as f:
+       for i in f:
+           if i == "\n":
+               continue
+           i_strip = i.rstrip("\n")
+           d = list(i_strip.split(','))
+           if d[1] == "" or d[1] == 'None':
+               d = data[-1]
+           elif d[0] == "" or d[0] == "None":
+               d = data[-1]
+           d[0] = float(d[0])
+           d[1] = float(d[1])
+           data.append(d)
+       data2.append(data[-1])
+   data3.append(data2[-1])
+
+   #print(f'Data to display: Moisture: {data3}')
+   #print(f'Data2 {data2}')
+   return render_template("results.html",data3 = data3, rain = rain, uv = uv)
 
 @app.route('/dashboard',methods = ['POST', 'GET'])
 def dashboard():
     data = []
     data_uv = []
-    with open('data2.txt') as f:
+    with open('data.txt') as f:
         for i in f:
             if i == "\n":
                 continue
@@ -70,25 +76,18 @@ def dashboard():
             if d[1] == "" or d[1] == 'None' or d[0] == "" or d[0] == "None":
                 continue
             d[1] = float(d[1])
+            d[0] = float(d[0])
             data.append(d)
-    with open('data3.txt') as f:
-        for i in f:
-            if i == "\n":
-                continue
-            i_strip = i.rstrip("\n")
-            d = list(i_strip.split(','))
-            if d[1] == "" or d[1] == 'None' or d[0] == "" or d[0] == "None":
-                continue
-            d[1] = float(d[1])
-            data_uv.append(d)
-    labels = [row[0] for row in data]
-    values = [row[1] for row in data]
-    labels_uv = [row[0] for row in data_uv]
-    values_uv = [row[1] for row in data_uv]
-    print(f'Labels: {labels}')
-    print(f'Values: {values}')
-    print(f'Labels_UV: {labels_uv}')
-    print(f'Values_UV: {values_uv}')
+    labels = ["16:00", "16:01", "16:02", "16:03", "16:04", "16:05", "16:06", "16:07", "16:08", "16:09", "16:10"]
+    values = [row[0] for row in data]
+    values = values[-11:]
+    labels_uv = ["16:00", "16:01", "16:02", "16:03", "16:04", "16:05", "16:06", "16:07", "16:08", "16:09", "16:10"]
+    values_uv = [row[1] for row in data]
+    values_uv = values_uv[-11:]
+    #print(f'Labels: {labels}')
+    #print(f'Values: {values}')
+    #print(f'Labels_UV: {labels_uv}')
+    #print(f'Values_UV: {values_uv}')
     return render_template("dashboard.html", labels = labels, values = values, labels_uv = labels_uv, values_uv = values_uv)
 
 if __name__ == '__main__':
